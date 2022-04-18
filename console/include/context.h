249d3f786986e623176202edbb5d7b9b8c2649a7
Tomas Sanchez <tosanchez@frba.utn.edu.ar>
2022-04-18 10:51:50 -0300

    docs(console): add context doc

--- console/include/context.h
+++ console/include/context.h
@@ -3,11 +3,25 @@
 #define CONTEXT_AVAILABLE 0
 #define CONTEXT_UNAVAILABLE -1
 
-typedef struct context
+/**
+ * @brief Context of Console Module.
+ *
+ */
+typedef struct Context
 {
+	// The input file path.
 	char *input_file_name;
+	// The process size.
 	int process_size;
+	// The context status code.
 	int status;
 } context_t;
 
-context_t read_context(int argc, char *argv[]);
+/**
+ * @brief Evaluates the context of the console.
+ *
+ * @param argc the number of arguments passed to the console
+ * @param argv the arguments list
+ * @return a context object
+ */
+context_t context_evaluate(int argc, char *argv[]);
