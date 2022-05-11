21c1bcf3497b392930d2ad6f6df30b922e58243f
Tomas Sanchez <tosanchez@frba.utn.edu.ar>
2022-05-10 22:20:15 -0300

    fix(lib): add safe queue life-cycle

--- lib/inc/util/queue/safe_queue.h
+++ lib/inc/util/queue/safe_queue.h
@@ -24,12 +24,28 @@ typedef struct SafeQueue
 
 } safe_queue_t;
 
+/**
+ * @brief Creates a safe queue instance
+ *
+ * @return a new safe queue instance
+ */
+safe_queue_t *
+new_safe_queue(void);
+
+/**
+ * @brief Deallocates memory associated with a safe queue.
+ *
+ * @param queue to be deleted
+ * @param destroyer method used to deleted queue elements
+ */
+void safe_queue_destroy(safe_queue_t *queue,
+						void (*destroyer)(void *));
 
 /**
  * @brief safely add an element at the last of the queue.
- * 
+ *
  * @param queue the safe queue itself
- * 
+ *
  * @param element the element in order to add to the safe queue
  *
  * @return void
@@ -40,7 +56,7 @@ void safe_queue_push(safe_queue_t *queue, void *element);
  * @brief safely remove the last element of the queue.
  *
  * @param queue the safe queue itself
- * 
+ *
  * @return void
  */
 void safe_queue_pop(safe_queue_t *queue);
