9152c36c81e0c6ed2ada2a57898fd7989b112ee4
Tomas Sanchez <tosanchez@frba.utn.edu.ar>
2022-06-19 19:04:03 -0300

    style: log request handlers

--- memory/src/server/cpu_controller.c
+++ memory/src/server/cpu_controller.c
@@ -12,6 +12,7 @@
 #include "cpu_controller.h"
 #include "conexion.h"
 #include "server.h"
+#include "log.h"
 #include <time.h>
 
 /**
@@ -35,10 +36,21 @@ obtain_memory_value(uint32_t position);
 void cpu_controller_read(int socket)
 {
 	uint32_t memory_position = obtain_memory_position(socket);
+	LOG_TRACE("Reading position %d of Memory.", memory_position);
 
 	uint32_t value = obtain_memory_value(memory_position);
+	LOG_TRACE("Position read has value= %d.", value);
 
-	servidor_enviar_stream(RD, socket, &value, sizeof(value));
+	ssize_t bytes_sent = servidor_enviar_stream(RD, socket, &value, sizeof(value));
+
+	if (bytes_sent > 0)
+	{
+		LOG_DEBUG("Value <%d> was sent [%d bytes]", value, bytes_sent);
+	}
+	else
+	{
+		LOG_ERROR("Value could not be sent.");
+	}
 }
 
 // ============================================================================================================
--- memory/src/server/kernel_controller.c
+++ memory/src/server/kernel_controller.c
@@ -14,6 +14,7 @@
 #include "kernel_controller.h"
 #include "server.h"
 #include "pcb.h"
+#include "log.h"
 
 // ============================================================================================================
 //                                   ***** Declarations *****
@@ -37,9 +38,20 @@ void kernel_controller_swap(int socket)
 	ssize_t bytes_received = -1;
 	void *pcb_stream = servidor_recibir_stream(socket, &bytes_received);
 
+	LOG_TRACE("A PCB was received to be swapped");
 	uint32_t memory_position = swap_pcb(pcb_stream);
+	LOG_TRACE("PCB  was swapped to position <%d>", memory_position);
 
-	servidor_enviar_stream(SWAP, socket, &memory_position, sizeof(memory_position));
+	ssize_t bytes_sent = servidor_enviar_stream(SWAP, socket, &memory_position, sizeof(memory_position));
+
+	if (bytes_sent > 0)
+	{
+		LOG_DEBUG("Memory position <%d> was sent [%d bytes]", memory_position, bytes_sent);
+	}
+	else
+	{
+		LOG_ERROR("Memory position could not be sent.");
+	}
 }
 
 // ============================================================================================================
