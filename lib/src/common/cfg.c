3ee4415fdb927bef06c0dc7783349b28e3f5731d
fmaver <fmaver@frba.utn.edu.ar>
2022-07-05 00:07:24 -0300

    chau inline

--- lib/src/common/cfg.c
+++ lib/src/common/cfg.c
@@ -332,28 +332,28 @@ int entradas_tlb(void)
 
 #define TAM_MEMORIA "TAM_MEMORIA"
 
-inline int tam_memoria(void)
+int tam_memoria(void)
 {
 	return config_int(TAM_MEMORIA);
 }
 
 #define TAM_PAGINA "TAM_PAGINA"
 
-inline int tam_pagina(void)
+int tam_pagina(void)
 {
 	return config_int(TAM_PAGINA);
 }
 
 #define ENTRADAS_POR_TABLA "ENTRADAS_POR_TABLA"
 
-inline int entradas_por_tabla(void)
+int entradas_por_tabla(void)
 {
 	return config_int(ENTRADAS_POR_TABLA);
 }
 
 #define RETARDO_MEMORIA "RETARDO_MEMORIA"
 
-inline int retardo_memoria(void)
+int retardo_memoria(void)
 {
 	return config_int(RETARDO_MEMORIA);
 }
@@ -367,21 +367,21 @@ inline char *algoritmo_reemplazo(void)
 
 #define MARCOS_POR_PROCESO "MARCOS_POR_PROCESO"
 
-inline int marcos_por_proceso(void)
+int marcos_por_proceso(void)
 {
 	return config_int(MARCOS_POR_PROCESO);
 }
 
 #define RETARDO_SWAP "RETARDO_SWAP"
 
-inline int retardo_swap(void)
+int retardo_swap(void)
 {
 	return config_int(RETARDO_SWAP);
 }
 
 #define PATH_SWAP "PATH_SWAP"
 
-inline char *path_swap(void)
+char *path_swap(void)
 {
 	return config_string(PATH_SWAP);
 }
