9db1e4f5e5a8281ae4fea601a5d9ef796bf39d8e
fmaver <fmaver@frba.utn.edu.ar>
2022-07-06 20:51:38 -0300

    Se siguieron con las funciones de TLB

---
+++ cpu/include/module/tlb.h
@@ -0,0 +1,87 @@
+/**
+ * @file tlb.h
+ * @author Francisco Maver <fmaver@frba.utn.edu.ar>
+ * @brief
+ * @version 0.1
+ * @date 2022-07-06
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
+
+#pragma once
+
+#include "pcb.h"
+#include "thread_manager.h"
+#include "conexion.h"
+#include "server.h"
+#include "instruction.h"
+#include "sync.h"
+
+typedef struct TLB
+{
+	char* pagina;
+	char* frame;
+
+	char* bit_presencia;
+	char* tiempo_ult_acceso;
+
+	// TODO --> validar mirando la consigna si se pueden tener estos valores en la estructura del TLB
+	uint32_t tamanio;
+	uint32_t ultima_pos_liberada;
+
+} TLB_t;
+
+
+/**
+ * @brief Iniciar el TLB
+ *
+ */
+void init_TLB();
+
+/**
+ * @brief Reseteo la TLB: Todos los Bits de presencia a 0
+ *
+ */
+void reset_TLB();
+
+/**
+ * @brief Funcion de puntero para poder tener la TLB según el algoritmo
+ *
+ * @param param1
+ * @param param2
+ */
+void (*TLB_builder)(uint32_t param1, uint32_t param2);
+
+/**
+ * @brief Actualizo la TLB llamando al TLBbuilder que se ajustará al algoritmo correspondiente
+ *
+ * @param pagina
+ * @param marco
+ */
+void update_TLB(uint32_t pagina, uint32_t marco);
+
+/**
+ * @brief Se actualiza la TLB para el algoritmo FIFO
+ *
+ */
+void update_TLB_FIFO( uint32_t pagina,uint32_t marco);
+
+
+/**
+ * @brief Se actualiza la TLB para el algoritmo LRU
+ *
+ * @param pagina
+ * @param marco
+ */
+void update_TLB_LRU(uint32_t pagina, uint32_t marco);
+
+/**
+ * @brief
+ *
+ * @param numero_pagina
+ * @param marco
+ * @return true
+ * @return false
+ */
+bool hit_TLB(uint32_t numero_pagina, uint32_t* marco);
