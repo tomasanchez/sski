a706ac587c6a6dabe10ff795f939230ec8c53e77
R. Nicolás Savinelli <savinelli@ieee.org>
2022-07-09 23:19:37 -0300

    feat: add pcb swap functionality

--- memory/src/server/kernel_controller.c
+++ memory/src/server/kernel_controller.c
@@ -10,10 +10,16 @@
  */
 
 #include <time.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <string.h>
+#include <sys/mman.h>
 
 #include "kernel_controller.h"
 #include "server.h"
 #include "pcb.h"
+#include "cfg.h"
 #include "log.h"
 
 // ============================================================================================================
@@ -28,6 +34,14 @@
  */
 uint32_t
 swap_pcb(void *pcb_stream);
+
+/**
+ * @brief Deletes swapped file
+ *
+ * @param pcb_id to identfy the swapped_partition
+ */
+void
+delete_swapped_pcb(uint32_t pcb_id);
 
 /**
  * @brief Get a free page table index
@@ -47,19 +61,37 @@ void kernel_controller_swap(int socket)
 	void *pcb_stream = servidor_recibir_stream(socket, &bytes_received);
 	LOG_TRACE("[Server] :=> A PCB was received to be swapped");
 
-	uint32_t memory_position = swap_pcb(pcb_stream);
-	LOG_TRACE("[Server] :=> PCB  was swapped to position <%d>", memory_position);
+	uint32_t swap_status = swap_pcb(pcb_stream);
+	if(swap_status == SUCCESS) {
+		LOG_TRACE("[Server] :=> PCB was SUCCESSSFULLY swapped");
+	} else {
+		LOG_TRACE("[Server] :=> Failed to swap PCB");
+	}
 
-	ssize_t bytes_sent = servidor_enviar_stream(SWAP, socket, &memory_position, sizeof(memory_position));
+	ssize_t bytes_sent = servidor_enviar_stream(SWAP, socket, &swap_status, sizeof(swap_status));
 
 	if (bytes_sent > 0)
 	{
-		LOG_DEBUG(" [Server] :=> Memory position <%d> was sent [%ld bytes]", memory_position, bytes_sent);
+		LOG_DEBUG(" [Server] :=> Swap status message sent with size [%ld bytes]", bytes_sent);
 	}
 	else
 	{
-		LOG_ERROR("[Server] :=> Memory position could not be sent.");
+		LOG_ERROR("[Server] :=> Swap status message could not be sent.");
 	}
+
+	free(pcb_stream);
+}
+
+void kernel_controller_delete_swap_file(int socket)
+{
+	ssize_t bytes_received = -1;
+	
+	uint32_t * pcb_id = (uint32_t *) servidor_recibir_stream(socket, &bytes_received);
+	LOG_TRACE("[Server] :=> A PCB ID #%d was received", *pcb_id);
+
+	delete_swapped_pcb(*pcb_id);
+
+	free(pcb_id);
 }
 
 void kernel_controller_memory_init(int socket)
@@ -100,11 +132,59 @@ void kernel_controller_memory_init(int socket)
 //                                   ***** Private Functions *****
 // ============================================================================================================
 
+static bool file_exists(char *fname){
+  return access( fname, F_OK ) EQ 0;
+}
+
+static void delete_file(char *fname) {
+	if(file_exists(fname)) {
+		remove(fname);
+	}
+}
+
 uint32_t
 swap_pcb(void *pcb_stream)
 {
-	free(pcb_stream);
-	return (uint32_t)rand();
+	uint32_t status = ERROR;
+
+	pcb_t * pcb = pcb_from_stream(pcb_stream);
+
+	char path[MAX_CHARS] = "";
+
+	sprintf(path,"%s%s%d%s", path_swap(), "/", pcb->id, ".swap");
+
+	delete_file(path);
+
+	int fd = open(path, O_WRONLY | O_CREAT, 0666);
+
+	if (fd != -1) {
+		status = SUCCESS;
+
+		off_t pct_stream_size = (off_t) pcb_bytes_size(pcb);
+
+		ftruncate(fd, pct_stream_size);
+
+		void * file_address = mmap(NULL, pct_stream_size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);	
+
+		memcpy(file_address, pcb_stream, pct_stream_size);
+		msync(file_address, pct_stream_size, MS_SYNC);
+		munmap(file_address, pct_stream_size);
+	}
+		
+	close(fd);
+
+	free(pcb);
+
+	return status;
+}
+
+void
+delete_swapped_pcb(uint32_t pcb_id) {
+	char path[MAX_CHARS] = "";
+
+	sprintf(path,"%s%s%d%s", path_swap(), "/", pcb_id, ".swap");
+
+	delete_file(path);
 }
 
 uint32_t
