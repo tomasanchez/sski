daa20c2df85843532f85aa828c492c225007670f
Tomas Sanchez <tosanchez@frba.utn.edu.ar>
2022-07-21 23:46:35 -0300

    refactor(big_table): moved to page_table

--- memory/inc/module/page_table.h
+++ memory/inc/module/page_table.h
@@ -12,6 +12,7 @@
 #pragma once
 
 #include <inttypes.h>
+#include "log.h"
 #include "lib.h"
 
 #define INVALID_FRAME UINT32_MAX
@@ -57,3 +58,32 @@ page_table_lvl_1_t *new_page_table(size_t rows);
  * @return a page table reference
  */
 page_table_lvl_2_t *new_page_table_lvl2(size_t rows);
+
+/**
+ * @brief Creates a combination of all tables of LEVEL II in one
+ *
+ * @param memory the module
+ * @param table_index the table_index
+ * @return a joined table
+ */
+page_table_lvl_2_t **
+create_big_table(void *memory, uint32_t table_index);
+
+/**
+ * @brief Logs a Row of a BIG Table
+ *
+ */
+#define LOG_ENTRY(table, entry) LOG_INFO("\t%d\t|%d\t|%d\t|%d\t|%d\t|", entry, (table[entry])->frame, (table[entry])->use, (table[entry])->present, (table[entry])->modified)
+
+#define LOG_TABLE_HEADER() LOG_DEBUG("\tINDEX\t|Frame\t|U\t|P\t|M\t|")
+
+/**
+ * @brief Logs a Big Table
+ *
+ */
+#define LOG_TABLE(rows, table)              \
+	{                                       \
+		LOG_TABLE_HEADER();                 \
+		for (uint32_t i = 0; i < rows; i++) \
+			LOG_ENTRY(table, i);            \
+	}
