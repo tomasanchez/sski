a469759ca0a2d12349c1fe75d6f835902d5ad236
Tomas Sanchez <tosanchez@frba.utn.edu.ar>
2022-07-23 15:06:51 -0300

    Update fs.c

--- memory/src/module/fs.c
+++ memory/src/module/fs.c
@@ -11,6 +11,9 @@
 #include <time.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 #include <sys/mman.h>
 #include <string.h>
 #include <stdlib.h>
