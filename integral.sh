c9fa47daa75d743e1bc0fe8583a7c741766e4835
fmaver <fmaver@frba.utn.edu.ar>
2022-07-23 16:06:10 -0300

    fix sh

--- integral.sh
+++ integral.sh
@@ -1,7 +1,7 @@
 rm -fr ./config
 cp -r ./test/integral/config/ ./
-./build/console.out /home/tosanchez/Dev/sski/test/integral/INTEGRAL_1.txt 2048
-./build/console.out /home/tosanchez/Dev/sski/test/integral/INTEGRAL_2.txt 2048
-./build/console.out /home/tosanchez/Dev/sski/test/integral/INTEGRAL_3.txt 2048
-./build/console.out /home/tosanchez/Dev/sski/test/integral/INTEGRAL_4.txt 2048
-./build/console.out /home/tosanchez/Dev/sski/test/integral/INTEGRAL_5.txt 2048
+./build/console.out /home/utnso/tp-2022-1c-code-for-goOS/test/integral/INTEGRAL_1.txt 2048
+./build/console.out /home/utnso/tp-2022-1c-code-for-goOS/test/integral/INTEGRAL_2.txt 2048
+./build/console.out /home/utnso/tp-2022-1c-code-for-goOS/test/integral/INTEGRAL_3.txt 2048
+./build/console.out /home/utnso/tp-2022-1c-code-for-goOS/test/integral/INTEGRAL_4.txt 2048
+./build/console.out /home/utnso/tp-2022-1c-code-for-goOS/test/integral/INTEGRAL_5.txt 2048
