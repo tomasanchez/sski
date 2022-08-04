f74b7eb501ade4ccfe975d533a3d93cc4d673054
Tomas Sanchez <tosanchez@frba.utn.edu.ar>
2022-08-04 19:10:47 -0300

    test: typo

--- integral.sh
+++ integral.sh
@@ -1,6 +1,6 @@
 rm -fr ./config
 cp -r ./test/integral/config/ ./
-./build/console.out .test/integral/INTEGRAL_1.txt 2048
+./build/console.out ./test/integral/INTEGRAL_1.txt 2048
 ./build/console.out ./test/integral/INTEGRAL_2.txt 2048
 ./build/console.out ./test/integral/INTEGRAL_3.txt 2048
 ./build/console.out ./test/integral/INTEGRAL_4.txt 2048
