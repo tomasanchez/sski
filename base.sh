20eb4c90dcb3e8bcd8b585dae2943ed9b5d0b607
Tomas Sanchez <tosanchez@frba.utn.edu.ar>
2022-08-04 15:55:43 -0300

    refactor: test

--- base.sh
+++ base.sh
@@ -1,5 +1,5 @@
 rm -fr ./config
 cp -r ./test/base/config/ ./
-./build/console.out /home/utnso/tp-2022-1c-code-for-goOS/test/base/BASE_1.txt 10
-./build/console.out /home/utnso/tp-2022-1c-code-for-goOS/test/base/BASE_2.txt 20
-./build/console.out /home/utnso/tp-2022-1c-code-for-goOS/test/base/BASE_2.txt 20
+./build/console.out ./test/base/BASE_1.txt 10
+./build/console.out ./test/base/BASE_2.txt 20
+./build/console.out ./test/base/BASE_2.txt 20
